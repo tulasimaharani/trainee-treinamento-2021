74657a51a84daed4ab73b5773cfdf613d4305ca1
tulasimaharani <tulasimaharani15@gmail.com>
2021-03-07 13:30:49 -0300

    Adiciona mapeamento de entidades MeioContato e Endereco

--- src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisica.java
+++ src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisica.java
@@ -1,22 +1,44 @@
 package br.com.infox.treinamento.trainee.pessoafisica;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.PostLoad;
+import javax.persistence.PostRemove;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.validation.groups.Default;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+
 
 import lombok.Getter;
 import lombok.Setter;
@@ -35,7 +57,7 @@ public class PessoaFisica implements Serializable {
 
 	@NotNull
     @Size(min = 11, max = 11)
-    @Pattern(regexp = "[0-9]*", message = "Must contain only numbers")
+    @Pattern(regexp = "[0-9]+", message = "Must contain only numbers")
 	@Column(name = "cd_cpf")
     private String cpf;
 	
@@ -48,67 +70,100 @@ public class PessoaFisica implements Serializable {
     @NotNull
     @NotEmpty
     @Email
-    @Column(name = "ds_email")
+    @Transient
     private String email;
 
     @NotNull
     @Size(min = 10, max = 12)
     @Digits(fraction = 0, integer = 12)
-    @Column(name = "ds_phone_number")
+    @Transient
     private String phoneNumber;
 
     @NotNull
     @Column(name = "dt_birth_date")
     private Date birthDate;  
     
-    public Long getId() {
-		return id;
+	@OrderBy("tipoMeioContato ASC, contato ASC")
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "id_pessoa_fisica", nullable = false)
+	private List<MeioContato> meiosContato = new ArrayList<>(0);
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "tb_pessoa_endereco",
+		joinColumns = @JoinColumn(referencedColumnName = "id_pessoa_fisica", name = "id_pessoa_fisica_j"),
+		inverseJoinColumns = @JoinColumn(referencedColumnName = "id_endereco", name = "id_endereco_j")
+	)
+	private List<Endereco> enderecos = new ArrayList<>(0);
+
+    private boolean isStringBlank(String str) {
+		return Optional.ofNullable(str).map(String::trim).map(String::isEmpty).orElse(true);
 	}
     
-    public void setId(Long id) {
-		this.id = id;
+    @PostLoad
+	protected void aposRecuperar() {
+		for (Iterator<MeioContato> iterator = getMeiosContato().iterator(); iterator.hasNext();) {
+			MeioContato m = iterator.next();
+			switch (m.getTipoMeioContato()) {
+			case EM:
+				if (isStringBlank(getEmail())) {
+					setEmail(m.getContato());
+				}
+				break;
+			case TF:
+				if (isStringBlank(getPhoneNumber())) {
+					setPhoneNumber(m.getContato());
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}  
+    
+    @PrePersist
+    protected void antesPersistir() {
+    	MeioContato telefone = new MeioContato();
+    	telefone.setTipoMeioContato(TipoMeioContato.TF);
+    	telefone.setContato(getPhoneNumber());
+    	getMeiosContato().add(telefone);
+    	
+    	MeioContato email = new MeioContato();
+    	email.setTipoMeioContato(TipoMeioContato.EM);
+    	email.setContato(getEmail());
+    	getMeiosContato().add(email);
+    }
+    
+    @PreUpdate
+	protected void antesAtualizar() {
+		Set<ConstraintViolation<PessoaFisica>> validate = Validation.buildDefaultValidatorFactory().getValidator()
+				.validate(this, Default.class);
+		if (validate.isEmpty()) {
+			for (Iterator<MeioContato> iterator = getMeiosContato().iterator(); iterator.hasNext();) {
+				MeioContato m = iterator.next();
+				switch (m.getTipoMeioContato()) {
+				case EM:
+					m.setContato(getEmail());
+					break;
+				case TF:
+					m.setContato(getPhoneNumber());
+					break;
+				default:
+					break;
+				}
+			}
+		}
 	}
     
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	@PreRemove
+	protected void antesDeRemover() {
+		
 	}
 	
-	public Date getBirthDate() {
-		return birthDate;
-	}
-	
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+	@PostRemove
+	protected void aposRemover() {
 
+	}
+    
 	@Override
 	public int hashCode() {
 		final int prime = 31;
---
+++ src/main/java/br/com/infox/treinamento/trainee/pessoafisica/TipoMeioContato.java
@@ -0,0 +1,10 @@
+package br.com.infox.treinamento.trainee.pessoafisica;
+
+public enum TipoMeioContato {
+	/** E-MAIL */
+	EM, 
+	/** TELEFONE FIXO */
+	TF, 
+	/** TELEFONE MÓVEL */
+	TM
+}
